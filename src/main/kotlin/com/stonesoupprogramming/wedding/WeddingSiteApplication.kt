339117306329aff6422a916af86000c583ed21ec
Patrick Luck <stonesoup@Patricks-MacBook-Pro.local>
2017-07-01 10:21:55 -0400

    Initial Commit

---
+++ src/main/kotlin/com/stonesoupprogramming/wedding/WeddingSiteApplication.kt
@@ -0,0 +1,11 @@
+package com.stonesoupprogramming.wedding
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class WeddingSiteApplication
+
+fun main(args: Array<String>) {
+    SpringApplication.run(WeddingSiteApplication::class.java, *args)
+}
