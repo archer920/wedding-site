005c6cedf4474ec6dc2386949b6a31ce049581a2
Patrick Luck <stonesoup@Patricks-MacBook-Pro.local>
2017-07-21 13:16:30 -0400

    Index Page is Complete

--- src/main/resources/static/js/wedding.js
+++ src/main/resources/static/js/wedding.js
@@ -1,3 +1,20 @@
-// $(document).ready(function(){
-//     $('.carousel').carousel();
-// });
+$(document).ready(function(){
+    $('#banner').empty().load('/navbarLinks');
+
+    $('#carousel').on('slid.bs.carousel', function () {
+        var imageWidth = $('.active > img').width();
+        var newWidth = imageWidth * .80;
+
+        var label = $('.active').find('.carousel-label');
+        var labelWidth = label.width();
+
+        if(labelWidth > imageWidth){
+            label.width(newWidth);
+        }
+    });
+
+    // $('#carousel').find('.active > img').each(function(i, e){
+    //     var width = $(e);
+    //     $('.carousel-label').width(width * .90);
+    // });
+});
